f79452f84b9ea53261d4ddbd8cdd105252338f27
steve911-prog <galja2908@gmail.com>
2022-09-05 23:30:24 +0200

    Weather component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-      Weather App
+    <div className="container-fluid my-app">
+      <img
+        className="img-fluid Back"
+        src="https://images.unsplash.com/photo-1512586175314-701bd44f8440?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1997&q=80"
+        alt=""
+      />
+      <Weather />
+      <footer className="mt-4">
+        <h4>
+          This project was coded by Halyna Biedarieva and is{" "}
+          <a href="https://github.com/HalynaB-2908/weather-react">
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://app.netlify.com/teams/galja2908/overview">Netlify</a>
+        </h4>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+export default function Weather() {
+    return (
+      <div className="Search rounded m-5">
+        <div className="row">
+          <div className="col-9">
+            <input type="search" className="form-control" placeholder="Type a city..."/>
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary"/>
+          </div>
+        </div>
+      </div>
+    );
+}
