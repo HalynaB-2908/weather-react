bfd2540da14821b389c3bd82bccb39350dbab5a4
steve911-prog <galja2908@gmail.com>
2022-09-08 11:22:08 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,28 +5,28 @@ export default function WeatherForecast() {
   return (
     <div className="row rounded border Values ms-5 mt-3 p-3">
       <div className="col-3 text-center ">
-        <div className="fs-3">Fri</div>
+        <div className="fs-4">Fri</div>
         <WeatherIcon code="01d" size={36} />
         <div className="Forecast-Temperature">
           <span>22°</span> <span>16°</span>
         </div>
       </div>
       <div className="col-3 text-center">
-        <div className="fs-3">Sat</div>
+        <div className="fs-4">Sat</div>
         <WeatherIcon code="04d" size={36} />
         <div className="Forecast-Temperature">
           <span>22°</span> <span>16°</span>
         </div>
       </div>
       <div className="col-3 text-center">
-        <div className="fs-3">Sun</div>
+        <div className="fs-4">Sun</div>
         <WeatherIcon code="11d" size={36} />
         <div className="Forecast-Temperature">
           <span>22°</span> <span>16°</span>
         </div>
       </div>
       <div className="col-3 text-center">
-        <div className="fs-3">Mon</div>
+        <div className="fs-4">Mon</div>
         <WeatherIcon code="02d" size={36} />
         <div className="Forecast-Temperature">
           <span>22°</span> <span>16°</span>
